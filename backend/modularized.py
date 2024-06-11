355ec9958d0d6a01435a9bb8a412b6f7b46a2f9f
marichu99 <martinmaati31@gmail.com>
2024-06-11 21:14:42 +0300

    "Added import of math module, created dictionaries for storing high and low prices for different currency pairs and timeframes, and implemented new functions for data preprocessing and analysis."

--- backend/modularized.py
+++ backend/modularized.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np    
 from threading import Thread
 import sys
+import math
 import json
 
 
@@ -15,6 +16,18 @@ TIMEFRAME = mt5.TIMEFRAME_M15
 high_TIMEFRAME=[mt5.TIMEFRAME_M30,mt5.TIMEFRAME_M15,mt5.TIMEFRAME_H1,mt5.TIMEFRAME_H4]
 lower_TIMEFRAMES=[mt5.TIMEFRAME_M1,mt5.TIMEFRAME_M5,mt5.TIMEFRAME_M15,mt5.TIMEFRAME_M30]
 NUM_BARS=1000
+
+# window formation
+xauusd_Dict ={1:{"highPrice":[],"lowPrice":[]},5:{"highPrice":[],"lowPrice":[]},15:{"highPrice":[],"lowPrice":[]},30:{"highPrice":[],"lowPrice":[]}}
+eurusd_Dict ={1:{"highPrice":[],"lowPrice":[]},5:{"highPrice":[],"lowPrice":[]},15:{"highPrice":[],"lowPrice":[]},30:{"highPrice":[],"lowPrice":[]}}
+usdcad_Dict ={1:{"highPrice":[],"lowPrice":[]},5:{"highPrice":[],"lowPrice":[]},15:{"highPrice":[],"lowPrice":[]},30:{"highPrice":[],"lowPrice":[]}}
+usdjpy_Dict ={1:{"highPrice":[],"lowPrice":[]},5:{"highPrice":[],"lowPrice":[]},15:{"highPrice":[],"lowPrice":[]},30:{"highPrice":[],"lowPrice":[]}}
+audcad_Dict ={1:{"highPrice":[],"lowPrice":[]},5:{"highPrice":[],"lowPrice":[]},15:{"highPrice":[],"lowPrice":[]},30:{"highPrice":[],"lowPrice":[]}}
+gbpusd_Dict ={1:{"highPrice":[],"lowPrice":[]},5:{"highPrice":[],"lowPrice":[]},15:{"highPrice":[],"lowPrice":[]},30:{"highPrice":[],"lowPrice":[]}}
+gbpjpy_Dict ={1:{"highPrice":[],"lowPrice":[]},5:{"highPrice":[],"lowPrice":[]},15:{"highPrice":[],"lowPrice":[]},30:{"highPrice":[],"lowPrice":[]}}
+eurjpy_Dict ={1:{"highPrice":[],"lowPrice":[]},5:{"highPrice":[],"lowPrice":[]},15:{"highPrice":[],"lowPrice":[]},30:{"highPrice":[],"lowPrice":[]}}
+
+
 
 def conn():
     # start the connection to MT5
@@ -41,17 +54,77 @@ def conn():
 
 def gatherDataController():
     print("We are gathering the data")
-    global lower_TIMEFRAMES,high_TIMEFRAME,SYMBOLS
+    global  er_TIMEFRAMES,high_TIMEFRAME,SYMBOLS
     for pair in SYMBOLS:
-        for timeframe in lower_TIMEFRAMES:
+        for timeframe in  lower_TIMEFRAMES:
             backtest_data = mt5.copy_rates_from_pos(pair,timeframe,1,NUM_BARS)
             bars = pd.DataFrame(backtest_data)
             # save the data into a dataframe
             comprehensive_name=f"{str(pair)}{str(timeframe)}"
-            bars.to_csv(f"/backtest/{comprehensive_name}.csv")
+            bars.to_csv(rf"backend\backtest\{comprehensive_name}.csv")
+            # pass the dataframe to another method for further pre-processing 
+            splitAndPreprocess(bars,str(pair),timeframe)
 
-            print(comprehensive_name)
-        
+def splitAndPreprocess(df,pair,timeframe):
+    df= pd.DataFrame(df)
+    # change the time variable
+    df["time"]=pd.to_datetime(df["time"],unit="s")
+    if(pair == "XAUUSD"):
+        print("The size of the dataframe",df.shape[0])
+        num_splits = df.shape[0]/100
+        split_dfs=np.array_split(df,num_splits)
+        for i,split_df in enumerate(split_dfs):
+            print("The timeframe",timeframe)
+            print(f"The {i+1} dataframe is {split_df}")
+            getHighLowPricesPerSplitDf(split_df,pair,timeframe)
+
+def getHighLowPricesPerSplitDf(split_df,pair,timeframe):
+    split_df=pd.DataFrame(split_df)
+    highClosePrice=math.floor(split_df["close"].max())
+    lowClosePrice=math.floor(split_df["close"].min())
+    highPrice =math.floor(split_df["high"].max())
+    lowPrice=math.floor(split_df["low"].min())
+
+    # wick analysis
+    bearish_PinBar=highPrice-highClosePrice
+    bullish_PinBar=lowPrice-lowClosePrice
+
+    print(f"the {pair} has a max price of {highClosePrice} and a low price of {lowClosePrice} at the {timeframe} minute timeframe")
+    if(pair == "XAUUSD"):
+        xauusd_Dict[timeframe]["highPrice"].append(highClosePrice)
+        xauusd_Dict[timeframe]["lowPrice"].append(lowClosePrice)
+    elif(pair == "EURUSD"):
+        eurusd_Dict[timeframe]["highPrice"].append(highClosePrice)
+        eurusd_Dict[timeframe]["lowPrice"].append(lowClosePrice)
+    elif(pair == "USDCAD"):
+        usdcad_Dict[timeframe]["highPrice"].append(highClosePrice)
+        usdcad_Dict[timeframe]["lowPrice"].append(lowClosePrice)
+    elif(pair == "USDJPY"):
+        usdjpy_Dict[timeframe]["highPrice"].append(highClosePrice)
+        usdjpy_Dict[timeframe]["lowPrice"].append(lowClosePrice)
+    elif(pair == "AUDCAD"):
+        audcad_Dict[timeframe]["highPrice"].append(highClosePrice)
+        audcad_Dict[timeframe]["lowPrice"].append(lowClosePrice)
+    elif(pair == "GBPUSD"):
+        gbpusd_Dict[timeframe]["highPrice"].append(highClosePrice)
+        gbpusd_Dict[timeframe]["lowPrice"].append(lowClosePrice)
+    elif(pair == "GBPJPY"):
+        gbpjpy_Dict[timeframe]["highPrice"].append(highClosePrice)
+        gbpjpy_Dict[timeframe]["lowPrice"].append(lowClosePrice)
+    elif(pair == "EURJPY"):
+        eurjpy_Dict[timeframe]["highPrice"].append(highClosePrice)
+        eurjpy_Dict[timeframe]["lowPrice"].append(lowClosePrice)
+    
+    for counter,price in enumerate(xauusd_Dict[30]["highPrice"]):
+        if(price in xauusd_Dict[30]["lowPrice"] or price in xauusd_Dict[15]["highPrice"]):
+            print("We have a point")
+            
+    
+
+    # print("The gold dictionary")
+    # print(f"The {timeframe} timeframe has a maximum price of {highClosePrice} and minimum price of {lowClosePrice}")
+    # print(xauusd_Dict)
+
 def main():
     conn()
 
